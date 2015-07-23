cf41bcc450d94f86cb0fe9cd0df53d98bbe22ef1
j-bron <janbroniowski@gmail.com>
2015-07-23 15:28:44 +0100

    functionality by frontend

---
+++ static/app.js
@@ -0,0 +1,65 @@
+/**
+ * Created by jas on 2015-07-23.
+ */
+angular.module('whatsyourgetApp', [])
+    .controller("currencyRatesController", function ($scope, $http) {
+        $scope.appState = 'init';
+        $scope.calculation = {};
+        $scope.loadCountriesData = function () {
+            var loadCountriesIsoCodes = function () {
+                    $http.get("data/coutriesIsoCodes.json").success(function (data) {
+                        $scope.countriesIsoCodes = data;
+                        loadActiveCountriesTaxData();
+                    });
+                },
+                loadActiveCountriesTaxData = function () {
+                    $http.get("data/activeCountriesTaxData.json").success(function (data) {
+                        $scope.activeCountriesTaxData = data;
+                        _.each($scope.activeCountriesTaxData, function (item) {
+                            var iso = _.findWhere($scope.countriesIsoCodes, {"ISO3166-1-Alpha-3": item.code});
+                            item.name = iso.name;
+                            item.currencyCode = iso.currency_alphabetic_code;
+                        })
+                    });
+                };
+            loadCountriesIsoCodes();
+        };
+        $scope.loadCountriesData();
+        $scope.calculate = function () {
+            var calcSalaryDetails = function (salaryGross, tax, fixedCost, rate) {
+                var salaryGrossExchange = salaryGross * rate,
+                    fixedCostExchange = fixedCost * rate,
+                    tax = (salaryGrossExchange - fixedCostExchange) * tax * 0.01,
+                    salaryNet = salaryGrossExchange - tax - fixedCostExchange;
+                if (salaryNet < 0) {
+                    salaryNet = 0;
+                }
+                if (tax < 0) {
+                    tax = 0;
+                }
+                return {
+                    salaryNet: s.numberFormat(salaryNet, 2),
+                    tax: s.numberFormat(tax, 2),
+                    fixedCost: s.numberFormat(fixedCostExchange, 2)
+                };
+            };
+
+            if ($scope.userCurrencyChoice.currencyCode == "PLN") {
+                $scope.calculation = calcSalaryDetails($scope.userSalary, $scope.userCurrencyChoice.tax, $scope.userCurrencyChoice.fixedCost, 1);
+                $scope.appState = 'calculationReady';
+            } else {
+                $http.get('http://api.fixer.io/latest?base=' + $scope.userCurrencyChoice.currencyCode).
+                    success(function (data) {
+                        $scope.currencyRatesData = data;
+                        $scope.calculation = calcSalaryDetails($scope.userSalary, $scope.userCurrencyChoice.tax, $scope.userCurrencyChoice.fixedCost, $scope.currencyRatesData.rates["PLN"]);
+                        $scope.appState = 'calculationReady';
+                    });
+            }
+        };
+    })
+    .directive("salarycalculator", function () {
+        return {
+            restrict: "A",
+            templateUrl: "templates/currencyRatesTemplate.html"
+        };
+    });
